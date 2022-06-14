98b732eb5e01ae44c6fb2291f230cd2448d62712
Eric Herman <eric@freesa.org>
2022-06-14 10:08:06 +0200

    Only set PAGES_REPO_NWO if needed
    If a fork author wishes to set PAGES_REPO_NWO to something else,
    especially while testing, we should not over-write that.

--- script/test-with-link-check.sh
+++ script/test-with-link-check.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+# jekyll build defaults to "origin" unless PAGES_REPO_NWO is set
+# if there is no "origin" branch and PAGES_REPO_NWO is not set
+# then default to publiccodenet/standard
+if [ "_$(git remote | grep origin)_" != "_origin_" ] &&
+   [ "_${PAGES_REPO_NWO}_" == "__" ]; then
 export PAGES_REPO_NWO=publiccodenet/standard
+fi
 
 # Build the site
 bundle exec jekyll build
