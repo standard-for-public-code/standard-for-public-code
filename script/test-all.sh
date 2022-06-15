ece02d6a4f8fd3d82ab7eb71b5d15c79d654e19b
Eric Herman <eric@freesa.org>
2022-06-15 11:13:30 +0200

    print commands as they are executed

--- script/test-all.sh
+++ script/test-all.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# This is script is referenced by .travis.yml
+# This is script is provided for local development,
+# it is not used by continuous integration
+
+# $ help set
+#      -x  Print commands and their arguments as they are executed.
+set -x
 
 ./script/test-markdown.sh &&
 ./script/test-with-link-check.sh &&
