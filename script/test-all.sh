e15c89b13d00a607b5f854c1d9e1979906dacdb0
Eric Herman <eric@freesa.org>
2022-06-15 11:14:50 +0200

    replace "&&" with "set -e"

--- script/test-all.sh
+++ script/test-all.sh
@@ -4,9 +4,11 @@
 # it is not used by continuous integration
 
 # $ help set
+#      -e  Exit immediately if a command exits with a non-zero status.
 #      -x  Print commands and their arguments as they are executed.
 set -x
+set -e
 
-./script/test-markdown.sh &&
-./script/test-with-link-check.sh &&
+./script/test-markdown.sh
+./script/test-with-link-check.sh
 ./script/check-new-links.sh
