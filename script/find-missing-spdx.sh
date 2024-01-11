b2980261df8fed4bb2a08f4d400942ddea61c565
Eric Herman <eric@freesa.org>
2024-01-11 15:54:25 +0100

    SPDX-FileCopyrightText 2024 alignment
    A few of these were missed in 9135425db444c1e18860a99963584da9c19a87f3,
    they are now of a more similar form, and less likely to be missed in
    the future.

--- script/find-missing-spdx.sh
+++ script/find-missing-spdx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2022 The Foundation for Public Code <info@publiccode.net>, https://standard.publiccode.net/AUTHORS
+# SPDX-FileCopyrightText: 2022-2024 The Foundation for Public Code <info@publiccode.net>, https://standard.publiccode.net/AUTHORS
 
 function git_list_files() {
 		# -r Recurse into sub-trees.
